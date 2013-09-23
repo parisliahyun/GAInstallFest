b4da4d0415fc44ab5eaf1a89b231f1b154f21c80
My Name <myemail@example.com>
2013-09-23 07:22:45 -0400

    add an S to message

--- welcome_app.rb
+++ welcome_app.rb
@@ -12,20 +12,20 @@ end
 
 post '/submit' do
 
-  @account_sid = ""
-  @auth_token = ""
+  @account_sid = "AC9f9bcf2380073817590d261416b71719"
+  @auth_token = "0f712b60d3959dc1fbdf9257a478dfcd"
 
   @client = Twilio::REST::Client.new(@account_sid, @auth_token)
 
   @account = @client.account
-  @message = @account.sms.messages.create(
+  @messages = @account.sms.messages.create(
     {
-      :from => '',
-      :to => '',
+      :from => '+19145954152',
+      :to => '+19144003994',
       :body => "#{params[:name]}'s installfest went #{params[:answer]}."
     }
   )
 
-  flash[:notice] = "SMS sent: #{@message}"
+  flash[:notice] = "SMS sent: #{@messages}"
   redirect '/'
 end
